333b736bbcdd4495957033eb27d4fbbd64db3963
Zehedina Smajlovic <usatransport16@gmail.com>
2022-10-24 22:59:52 -0600

    commit tree f01a2a3de495970eb57344d54a2b8d281380c518

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {}
+employee.name = "";
+employee.streetAdress = "";
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newUpdateEmployeeWithKeyAndValue = {...employee};
+
+    newUpdateEmployeeWithKeyAndValue[key] = value;
+    return newUpdateEmployeeWithKeyAndValue;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee;
+}
+
+
+
+
